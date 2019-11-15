d8aea9274943e0beb0a372d56eafa6a06f296760
Tam Dao <11000026+tamminhdao@users.noreply.github.com>
2019-11-15 14:17:19 -0600

    First commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render, cleanup, fireEvent } from "@testing-library/react"
+import React from "react"
+import ReactDOM from "react-dom"
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import App from "./App"
+
+it("renders without crashing", async () => {
+  const component = render(<App />)
+
+  const usernameInput = await component.findByLabelText("username")
+  const passwordInput = await component.findByLabelText("password")
+
+  fireEvent.input(usernameInput, { target: { value: "new-user" } })
+  fireEvent.input(passwordInput, { target: { value: "apassword" } })
+  fireEvent.submit(await component.findByText("Sign in"))
+  // component.debug()
+
+  await component.findByText("Hello new-user")
+
+  expect(component.queryByTestId("login")).toBeNull()
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+
+import { Shell } from "components"
 
 const App: React.FC = () => {
+  const [username, setUsername] = React.useState("")
+  const [loggedIn, setLoggedIn] = React.useState(false)
+
+  const signIn = () => setLoggedIn(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Shell>
+      {loggedIn ? (
+        <>Hello {username}</>
+      ) : (
+        <form data-testid="login" onSubmit={signIn}>
+          <label>
+            username
+            <input
+              value={username}
+              onChange={event => setUsername(event.target.value)}
+            />
+          </label>
+          <label>
+            password
+            <input></input>
+          </label>
+          <button>Sign in</button>
+        </form>
+      )}
+    </Shell>
+  )
 }
 
-export default App;
+export default App
